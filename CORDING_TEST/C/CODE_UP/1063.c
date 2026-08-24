ca6c2dc405b792d49755eb90cc61948211089ca2
basicymj19428-ai <basicymj19428@gmail.com>
2026-08-25 00:12:08 +0900

    code up

---
+++ CORDING_TEST/C/CODE_UP/1063.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+
+int main(void)
+{
+    int num1, num2;
+    int result = 0;
+
+    scanf("%d%d", &num1, &num2);
+
+    result = num1 > num2 ? num1 : num2;
+
+    printf("%d\n", result);
+    
+    return 0;
+}
