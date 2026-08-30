d51173ec06931c5391ee630f99131e0eb75bb9c9
basicymj19428-ai <basicymj19428@gmail.com>
2026-08-30 19:52:58 +0900

    python

---
+++ Python/혼공파/2장/format03.py
@@ -0,0 +1,9 @@
+output_f = "{:+d}".format(52)  #양수
+output_g = "{:+d}".format(-52)  #음수
+output_h = "{: d}".format(52)  #양수(기호부분 공백)
+output_i = "{: d}".format(-52)  #음수(기호부분 공백)
+
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
